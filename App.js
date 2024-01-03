3b5a819e382ca288550f20a07004603105013925
Günbay Sali <gunbayx@hotmail.de>
2024-01-03 17:43:31 +0100

    Countable

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'; 
+import { Button, Text, View, StyleSheet, Image} from 'react-native'; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+ 
+const App = () => { 
+  const [pressedCount, setPressedCount] = useState(0); 
 
+  return ( 
+    <View style={styles.container}> 
+      <Text style={styles.text}> 
+        {pressedCount > 0 ? `The button was pressed ${pressedCount} times! ` : 'The button isn\'t pressed yet'} 
+        {pressedCount >= 10 ? <Button title=" New Game " onPress={() => setPressedCount(0)} /> : " " }
+      </Text> 
+      
+      <Button 
+        style={styles.button}
+        title='Press me' 
+        onPress={() => setPressedCount(pressedCount + 1)}
+        disabled={pressedCount >=10 }
+      /> 
+
+      <Button 
+        style={styles.button}
+        title='Reset'
+        onPress={() => setPressedCount(0)}
+        disabled={pressedCount >=10 }
+      />
+      
+    </View> 
+
+)}; 
+
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#e6d4c1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  text: {
+    margin: 16,
+  },
+  button: {
+    width: 100,
+    color: "#841584",
+  },
 });
+
+export default App; 
